d3fca4a8fa03ad9e22d394ca4532422c15d7c626
bahittinn <bahittin84@gmail.com>
2023-07-18 09:11:39 +0300

    Initial Commit

---
+++ CardApp/Card.swift
@@ -0,0 +1,42 @@
+//
+//  Card.swift
+//  CardApp
+//
+//  Created by Bahittin on 17.07.2023.
+//
+
+import UIKit
+
+struct Card {
+    static var allValues = [
+        UIImage(named: "2H")!,
+        UIImage(named: "3H")!,
+        UIImage(named: "4H")!,
+        UIImage(named: "5H")!,
+        UIImage(named: "6H")!,
+        UIImage(named: "7H")!,
+        UIImage(named: "8H")!,
+        UIImage(named: "9H")!,
+        UIImage(named: "10H")!,
+        UIImage(named: "JH")!,
+        UIImage(named: "QH")!,
+        UIImage(named: "KH")!,
+        UIImage(named: "AH")!,
+        
+        UIImage(named: "2S")!,
+        UIImage(named: "3S")!,
+        UIImage(named: "4S")!,
+        UIImage(named: "5S")!,
+        UIImage(named: "6S")!,
+        UIImage(named: "7S")!,
+        UIImage(named: "8S")!,
+        UIImage(named: "9S")!,
+        UIImage(named: "10S")!,
+        UIImage(named: "JS")!,
+        UIImage(named: "QS")!,
+        UIImage(named: "KS")!,
+        UIImage(named: "AS")!
+        
+        
+    ]
+}
